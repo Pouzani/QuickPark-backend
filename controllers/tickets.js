e123e63d9562b4c35daf2be9f9162479b4c719eb
Soumya Kaddouri <75394110+soumyaKaddouri@users.noreply.github.com>
2022-06-01 15:34:05 +0000

    add get all tickets AND get ticket by id

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -23,3 +23,39 @@ exports.addTicket = async(req,res,next)=>{
         next();
     }
 }
+
+exports.getTickets = async(req,res,next) =>{
+    try {
+
+        let {userId,carId} =  req.params
+        const queryToGetAllTickets = query(collection(db, 'users',userId,'cars',carId,'tickets'));
+        const querySnapshot = await getDocs(queryToGetAllTickets);
+        let tickets = [];
+        querySnapshot.forEach((doc)=>{
+            let ticket = new Ticket(doc.data().arrivalTime,doc.data().leavingTime,doc.data().price,doc.data().review,doc.data().review,doc.data().feedback,doc.data().parkingId);
+            tickets.push(ticket.data);
+        })
+
+        res.status(200).json({success:true,operation:"Get all tickets",count:tickets.length,data:tickets})
+        
+    } catch (error) {
+        req.quickpark = {errorCode:error.code};
+        next();
+        
+    }
+    
+}
+
+exports.getTicket = async(req,res,next) =>{
+    try {
+        let {ticketId} =  req.params;
+        let {userId,carId} = req.body;
+        const docRef = doc(db, 'users', userId,'cars',carId,'tickets',ticketId);
+        const docSnap = await getDoc(docRef);
+        res.status(200).json({success:true,operation:"Get ticket by id",count:(docSnap.data()?1:0),data:docSnap.data()})
+        
+    } catch (error) {
+        req.quickpark = {errorCode:error.code};
+        next();
+    }
+}
--- routes/tickets.js
+++ routes/tickets.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const {addTicket} = require('../controllers/tickets');
+const {addTicket, getTickets, getTicket} = require('../controllers/tickets');
 
-router.post("/:userId/:carId",addTicket);
+router.post("/:userId/:carId",addTicket).get("/:userId/:carId", getTickets).get("/:ticketId",getTicket);
 
 module.exports = router;
